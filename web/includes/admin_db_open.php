5ae7e8e94ac4666303cc5be837d4c86a7a9809eb
Mark Moxon <mark@moxon.net>
2023-02-04 15:39:54 +0000

    Updated charset PDO connector syntax

--- web/includes/admin_db_open.php
+++ web/includes/admin_db_open.php
@@ -1,7 +1,7 @@
 <?php
   try {
 
-    $dbh  = new PDO("mysql:host=".ADMIN_DB_HOST.";dbname=".ADMIN_DB_NAME,ADMIN_DB_USER,ADMIN_DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
+    $dbh  = new PDO("mysql:host=".ADMIN_DB_HOST.";dbname=".ADMIN_DB_NAME.";charset=utf8",ADMIN_DB_USER,ADMIN_DB_PASS);
   } catch (PDOException $e) {
     throw new PDOException("Error  : " .$e->getMessage());
 echo "error";
