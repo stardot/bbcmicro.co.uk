091667b9cb253bac0824232540cd794525567adc
Mark Moxon <mark@moxon.net>
2023-02-16 15:42:31 +0000

    Delete obsolete content from tmp2

--- web/tmp2/jsbteletext/app/preload.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-window.nodeRequire = require
-
-window.addEventListener('DOMContentLoaded', () => {
-  for (const node of document.getElementsByClassName('not-electron')) {
-    node.remove()
-  }
-})
