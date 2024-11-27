fed741c5aa722402da4f08563ddf2afcc82227de
Mark Moxon <mark@moxon.net>
2024-11-27 11:30:48 +0000

    Make PDO connection consistent

--- web/includes/db_connect.php
+++ web/includes/db_connect.php
@@ -1,6 +1,6 @@
 <?php
   try {
-    $db  = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",DB_USER,DB_PASS);
+    $db  = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",DB_USER,DB_PASS, array(PDO::ATTR_PERSISTENT => true));
   } catch (PDOException $e) {
     throw new PDOException("Error  : " .$e->getMessage());
 echo "error";
